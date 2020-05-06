6f68300cdbe2c9f2e0c35ebcb4d9eac6d8309fed
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-06 15:47:02 +0900

    add category and area menu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
+  before_action :set_common_info
+
+  def set_common_info
+    @all_areas = Area.all
+    @all_categorys = Category.all
+  end
 end
