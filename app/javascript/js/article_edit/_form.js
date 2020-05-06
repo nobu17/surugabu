488868e13bb4d88636a56ee1760934fe6709f6a8
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-07 02:14:56 +0900

    fix image preview

--- app/javascript/js/article_edit/_form.js
+++ app/javascript/js/article_edit/_form.js
@@ -1,30 +1,45 @@
-import SimpleMDE from 'simplemde'
+import SimpleMDE from "simplemde";
 
 $(document).on("turbolinks:load", function () {
-
+  console.log("load");
   // markdown editor activate
-  console.log("editor")
-  const simplemde = new SimpleMDE({ element: document.getElementById("editor") });
+  const simplemde = new SimpleMDE({
+    element: document.getElementById("editor"),
+  });
 
-  // add image preview display change function 
+  // add image preview display change function
   $("#article_title_image").on("change", function (e) {
+    console.log("change");
+    // reset preview area
+    onChangeImage("#image_preview", e);
+  });
+  // add image preview display change function
+  $("#article_content_images").on("change", function (e) {
+    console.log("change2");
+    // reset preview area
+    onChangeImage("#image_previews", e);
+  });
+
+  const onChangeImage = function (previewArea, e) {
+    // reset preview area
+    $(previewArea).empty();
     const files = e.target.files;
     let d = new $.Deferred().resolve();
     $.each(files, function (i, file) {
       d = d.then(function () {
-        return previewImage(file);
+        return previewImage(previewArea, file);
       });
     });
-  });
+    // to detect change event everytime
+    $(previewArea).val("");
+  };
 
-  const previewImage = function (imageFile) {
+  const previewImage = function (previewArea, imageFile) {
     const reader = new FileReader();
     const img = new Image();
     const def = $.Deferred();
     reader.onload = function (e) {
-      // 画像を表示
-      $("#image_preview").empty();
-      $("#image_preview").append(img);
+      $(previewArea).append(img);
       img.src = e.target.result;
       def.resolve(img);
     };
