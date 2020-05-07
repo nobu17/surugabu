2bbf59278ebffedecfea68413a0eb199c4665bb6
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-07 21:21:44 +0900

    change layout

--- app/javascript/js/article_edit/_form.js
+++ app/javascript/js/article_edit/_form.js
@@ -2,8 +2,12 @@ import SimpleMDE from "simplemde";
 
 let simplemde;
 
-window.onload = function() {
-  console.log("load");
+window.onload = function () {
+  loadMDE();
+  addImagePreviewEvent();
+};
+
+function loadMDE() {
   const editor = document.getElementById("editor");
   if (!simplemde && editor) {
     console.log("add");
@@ -11,7 +15,9 @@ window.onload = function() {
       element: editor,
     });
   }
+}
 
+function addImagePreviewEvent() {
   // add image preview display change function
   $("#article_title_image").on("change", function (e) {
     onChangeImage("#image_preview", e);
@@ -48,57 +54,3 @@ window.onload = function() {
     return def.promise();
   };
 }
-
-$(document).on("render", function () {
-  // markdown editor activate
-  console.log("render");
-});
-
-$(document).on("load", function () {
-  // markdown editor activate
-  alert("load");
-  console.log("load");
-  editor = document.getElementById("editor");
-  if (!simplemde && editor) {
-    console.log("add");
-    simplemde = new SimpleMDE({
-      element: editor,
-    });
-  }
-
-  // add image preview display change function
-  $("#article_title_image").on("change", function (e) {
-    onChangeImage("#image_preview", e);
-  });
-  // add image preview display change function
-  $("#article_content_images").on("change", function (e) {
-    onChangeImage("#image_previews", e);
-  });
-
-  const onChangeImage = function (previewArea, e) {
-    // reset preview area
-    $(previewArea).empty();
-    const files = e.target.files;
-    let d = new $.Deferred().resolve();
-    $.each(files, function (i, file) {
-      d = d.then(function () {
-        return previewImage(previewArea, file);
-      });
-    });
-    // to detect change event everytime
-    $(previewArea).val("");
-  };
-
-  const previewImage = function (previewArea, imageFile) {
-    const reader = new FileReader();
-    const img = new Image();
-    const def = $.Deferred();
-    reader.onload = function (e) {
-      $(previewArea).append(img);
-      img.src = e.target.result;
-      def.resolve(img);
-    };
-    reader.readAsDataURL(imageFile);
-    return def.promise();
-  };
-});
