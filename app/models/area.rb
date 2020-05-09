402003928bbc1ed102ee3e67a0caa7f0190d7a8b
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-09 22:38:53 +0900

    fix edit layout

--- app/models/area.rb
+++ app/models/area.rb
@@ -4,7 +4,9 @@ class Area < ApplicationRecord
   has_many :article_areas
   has_many :aricles, through: :article_areas
 
-  validates :display_order, numericality: { only_integer: true, greater_than: 0, less_than: 999  }
+  validates :display_order, numericality: { only_integer: true, greater_than: 0, less_than: 999 }
+  validates :name, presence: true
+  validates :name, length: { minimum: 1, maximum: 10, too_long: '最大%{count}文字まで使えます' }
 
   scope :get_areas_orderby_display_order, lambda {
     all.order(display_order: :desc)
--- app/models/category.rb
+++ app/models/category.rb
@@ -4,7 +4,9 @@ class Category < ApplicationRecord
   has_many :article_categorys
   has_many :aricles, through: :article_categorys
 
-  validates :display_order, numericality: { only_integer: true, greater_than: 0, less_than: 999  }
+  validates :display_order, numericality: { only_integer: true, greater_than: 0, less_than: 999 }
+  validates :name, presence: true
+  validates :name, length: { minimum: 1, maximum: 10, too_long: '最大%{count}文字まで使えます' }
 
   scope :get_categorys_orderby_display_order, lambda {
     all.order(display_order: :desc)
