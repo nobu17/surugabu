ba12634dec07451397a11950f2aa13dea0307194
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-12 20:31:23 +0900

    fix command

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -7,8 +7,5 @@ Category.create(name: 'グルメ')
 Category.create(name: '観光')
 
 
-# user = User.new(:email => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'])
-# user.save!
-
-user = User.new(:email => 'hogehoge@hoge.com', :password => 'hugahuga')
+user = User.new(:email => ENV['ADMIN_USER'], :password => ENV['ADMIN_PASSWORD'])
 user.save!
