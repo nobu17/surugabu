179060714b53904b0c9d536767c401bf9c88f460
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-18 00:38:02 +0900

    change device only available login/logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
   # map onlyfor login/logout
   # devise_for :users, skip: :all
   # devise_scope :user do
@@ -45,4 +44,10 @@ Rails.application.routes.draw do
   delete 'admin/category/:id', to: 'category#destroy'
   get 'admin/category/:id/edit', to: 'category#edit'
   get 'admin/category/new', to: 'category#new'
+
+  devise_scope :user do
+    get '/users/sign_in' => 'devise/sessions#new', as: :new_user_session
+    post '/users/sign_in' => 'devise/sessions#create', as: :user_session
+    delete '/users/sign_out' => 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
