c6974a450683af8d095e70c826f77f2140e56d70
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-05-18 23:45:27 +0900

    add quality

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -5,7 +5,7 @@ class Attachment < ApplicationRecord
 
   def image_compressed
     if image.attached?
-      image.variant(resize_to_fit: [1024, 1024]).processed
+      image.variant(resize_to_fit: [800, 800], quality: 100).processed
     end
   end
 end
