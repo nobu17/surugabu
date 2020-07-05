df0d8e5cce240f2821fee4dc436969cbd11bdc1c
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-07-05 14:51:24 +0900

    add comment

--- app/javascript/js/article_edit_show/index.js
+++ app/javascript/js/article_edit_show/index.js
@@ -6,15 +6,16 @@ function loadButtonEvents() {
   const btns = document.getElementsByClassName("twitter_btn");
   for (const btn of btns) {
     btn.onclick = function () {
-      openTwitter(btn.id);
+      console.log("btn", btn);
+      openTwitter(btn.id, btn.value);
     };
   }
 }
 
-function openTwitter(article_id) {
+function openTwitter(article_id, add_comment) {
   const tag = "駿河部,沼津市,富士市";
   const my_url = "https://" + window.location.host + "/articles/" + article_id;
-  const comment = "記事を投稿しました。 ";
+  const comment = "記事を投稿しました。 " + add_comment;
 
   url =
     "http://twitter.com/share?url=" +
