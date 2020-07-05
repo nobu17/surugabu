edb4eeff52e1ae24370af02d19366940a875e2a0
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-07-05 12:25:59 +0900

    fix image of ogp

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,13 +20,13 @@ module ApplicationHelper
         description: :description,
         type: 'website',
         url: request.original_url,
-        image: image_url('/assets/maguro.svg'),
+        image: image_url('/assets/magu.png'),
         local: 'ja-JP'
       },
       twitter: {
         card: 'summary',
         site: '@BuSuruga',
-        image: image_url('/assets/maguro.svg'),
+        image: image_url('/assets/magu.png'),
         width: 100,
         height: 100
       }
