e9670276d51aa44a43346806d10438ea90bd1b53
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-07-05 13:51:16 +0900

    add twitter post feature

--- app/javascript/js/article_edit_show/index.js
+++ app/javascript/js/article_edit_show/index.js
@@ -1,9 +1,27 @@
+window.onload = function () {
+  loadButtonEvents();
+};
+
+function loadButtonEvents() {
+  const btns = document.getElementsByClassName("twitter_btn");
+  for (const btn of btns) {
+    btn.onclick = function () {
+      openTwitter(btn.id);
+    };
+  }
+}
 
 function openTwitter(article_id) {
-    alert(article_id)
-    const my_url = "https://" + window.location.host + "/articles/" + article_id
-    const comment = "記事を投稿しました。#駿河部 #沼津" + my_url
+  const tag = "駿河部,沼津";
+  const my_url = "https://" + window.location.host + "/articles/" + article_id;
+  const comment = "記事を投稿しました。 " + my_url;
 
-    url = "http://twitter.com/share?url=" + escape(my_url) + "&text=" + comment;
-    window.open(url,"_blank","width=600,height=300");
+  url =
+    "http://twitter.com/share?url=" +
+    escape(document.location.href) +
+    "&text=" +
+    comment +
+    "&hashtags=" +
+    tag;
+  window.open(url, "_blank");
 }
