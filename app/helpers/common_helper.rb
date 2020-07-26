1472b4955d078bbd45c5135c329711ca10f54982
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-07-26 12:58:05 +0900

    fix image

--- app/helpers/common_helper.rb
+++ app/helpers/common_helper.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module CommonHelper
+  def image_with_default(image)
+    if image.present? && image.attached?
+      image_tag(image)
+    else
+      image_tag('no-image.png')
+    end
+  end
+
   def display_image(image, width = 400)
     if image.present? && image.attached?
       image_tag(image, width: width)
