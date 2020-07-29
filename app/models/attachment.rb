121772ffe77416515a205c924d7d15a01d50a550
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-07-29 23:09:22 +0900

    fix image attachment

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,13 +1,20 @@
 # frozen_string_literal: true
 
+# active sotrage. it will be deleted.
 class Attachment < ApplicationRecord
-  # has_one_attached :image
+  has_one_attached :image
+
+  def image_compressed
+    if image.attached?
+      image.variant(resize_to_fit: [800, 600], quality: 100).processed
+    end
+  end
+end
+
+class ImageAttachment < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   def image_compressed
-    # if image.attached?
-    #  image.variant(resize_to_fit: [800, 600], quality: 100).processed
-    # end
     image
   end
 end
