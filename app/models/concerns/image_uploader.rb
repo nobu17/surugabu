201443dd223f4ff6479891dfa75da30f5616e0fa
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-07-30 23:55:23 +0900

    remove image processing

--- app/models/concerns/image_uploader.rb
+++ app/models/concerns/image_uploader.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ImageUploader < CarrierWave::Uploader::Base
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
 
-  process resize_to_fit: [700, 700]
+  # process resize_to_fit: [700, 700]
 
   # test env upload loacal
   if Rails.env.development?
