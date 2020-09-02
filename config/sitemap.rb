aa4a6a5159affe4cb95936dbc534414f6aa75f0b
nobuhiro_inagawa <lunacy171710@yahoo.co.jp>
2020-09-03 00:36:25 +0900

    fix

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -5,7 +5,7 @@ require 'google/cloud/storage'
 
 SitemapGenerator::Sitemap.default_host = 'https://surugabu.com'
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::GoogleStorageAdapter.new(
-  credentials: ENV['GOOGLE_CREDENTIALS'].as_json,
+  credentials: ENV['GOOGLE_CREDENTIALS'],
   project_id: ENV['GCS_PROJECT_ID'],
   bucket: ENV['GCS_BUCKET']
 )
